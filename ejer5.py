3b057d550e24ac2f9e82ada38911341d673474bb
luis <lguerra009@gmail.com>
2022-08-26 13:08:07 -0300

    cambio en ejercicio6

---
+++ ejer5.py
@@ -0,0 +1,29 @@
+nombre: str = 'Marcos'
+apellido: str = 'villagra'
+edad = 35
+email= 'marcosv@gmail.com'
+telefono = 1145785242
+casado = False
+con_hijos = False
+lista_amigos = [
+    "jose",
+    "dario",
+    "ana",
+    "andrea"
+    ]
+
+pelicualas_favoritas = {
+    "clave1":"el senios de los anillos",
+    "clave2":"green book",
+    "clave3": "advengers"
+}
+
+print(nombre)
+print(apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(con_hijos)
+print(lista_amigos)
+print(pelicualas_favoritas)
---
+++ ejer6.py
@@ -0,0 +1,5 @@
+peso = float(input ("ingresa tu peso en KG "))
+altura = float(input("ingresa tu altura "))
+imc = round( peso/altura**2)
+
+print("tu indixez corporal es ", imc)
